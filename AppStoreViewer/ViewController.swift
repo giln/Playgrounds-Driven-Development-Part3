284b33a10252859c7281fc9ccbd9d2c188ff7004
Gil Nakache <gil.nakache@viseo.com>
2018-10-17 14:57:37 +0200

    Initial Commit

---
+++ AppStoreViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreViewer
+//
+//  Created by Gil Nakache on 17/10/2018.
+//  Copyright © 2018 Viseo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
